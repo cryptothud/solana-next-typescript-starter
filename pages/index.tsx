52b0aa9c3a55cf24611d6c0b1a088f707e0323d1
Thud <thuddesigns@gmail.com>
2023-03-31 04:07:22 -0700

    updated head tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,27 +8,35 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta name="twitter:title" content={projectName} />
-        <meta
-          name="twitter:description"
-          content={projectDescription}
-        />
-        <meta name="twitter:image" content={previewBannerURL} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <title>{projectName}</title>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
         <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="192x192" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="512x512" href="/favicon.ico" />
+
+        {/* Misc. */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#393E48" />
+        <meta name="theme-color" content="#FAF7F2" />
         <meta name="fortmatic-site-verification" content="j93LgcVZk79qcgyo" />
+
+        {/* Primary Meta Tags */}
+        <title>{projectName}</title>
+        <meta name="title" content={projectName} />
+        <meta name="description" content={projectDescription} />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
         <meta property="og:url" content="/" />
         <meta property="og:title" content={projectName} />
-        <meta
-          property="og:description"
-          content={projectDescription}
-        />
+        <meta property="og:description" content={projectDescription} />
         <meta property="og:image" content={previewBannerURL} />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="/" />
+        <meta property="twitter:title" content={projectName} />
+        <meta property="twitter:description" content={projectDescription} />
+        <meta property="twitter:image" content={previewBannerURL} />
       </Head>
       <div className="flex flex-col justify-center items-center">
         <DisplaySOL />
