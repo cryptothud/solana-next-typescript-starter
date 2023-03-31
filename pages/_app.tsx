ac754391ae6f872a2c2d1a508f6729a2f79c4304
Thud <thuddesigns@gmail.com>
2023-03-31 04:43:56 -0700

    aldflj

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,8 @@ import {
 require("@solana/wallet-adapter-react-ui/styles.css")
 import MainLayout from '../components/layouts/mainLayout'
 import { RecoilRoot } from 'recoil'
+import { Head } from 'next/document'
+import { projectName, projectDescription, previewBannerURL } from '../scripts/config'
 
 const WalletProvider = dynamic(
   () => import("../components/wallet/Wallet"),
@@ -29,14 +31,47 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <WalletProvider>
-          <MainLayout>
-            <Component {...pageProps} />
-          </MainLayout>
-        </WalletProvider>
-      </RecoilRoot>
-    </QueryClientProvider>
+    <>
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/favicon.ico" />
+
+        {/* Misc. */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#FAF7F2" />
+        <meta name="fortmatic-site-verification" content="j93LgcVZk79qcgyo" />
+
+        {/* Primary Meta Tags */}
+        <title>{projectName}</title>
+        <meta name="title" content={projectName} />
+        <meta name="description" content={projectDescription} />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="/" />
+        <meta property="og:title" content={projectName} />
+        <meta property="og:description" content={projectDescription} />
+        <meta property="og:image" content={previewBannerURL} />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="/" />
+        <meta property="twitter:title" content={projectName} />
+        <meta property="twitter:description" content={projectDescription} />
+        <meta property="twitter:image" content={previewBannerURL} />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <WalletProvider>
+            <MainLayout>
+              <Component {...pageProps} />
+            </MainLayout>
+          </WalletProvider>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
   )
 }
